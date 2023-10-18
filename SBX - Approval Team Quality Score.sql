7df3ce1ff58a4f5081680060c2637ab78250303b
Savas Basar Trak <trak.sb@gmail.com>
2023-10-18 21:32:50 +0200

    18.10.23 update

--- SBX - Approval Team Quality Score.sql
+++ SBX - Approval Team Quality Score.sql
@@ -11,18 +11,23 @@ with wc as (
 
 select
     s.partition_0
-    ,ac.user_id
+    ,sub.user_id
     ,s.session_id as sub_id
     ,s.score
     ,wc.max_answer_word_count
+    ,MAX(time_stamp) - MIN(time_stamp) as time_difference
     ,case
         when length(s.session_id) = 8 then 'https://admin.streetbees.com/projects/' || substr(s.partition_0, 1,5) || '/submissions/' || s.session_id
         else 'https://project-guiness-throwaway-production.streetbees.com/admin/projects/' || s.partition_0 || '/chats/' || s.session_id
     end as session_link
-from sbx_transcripts.answer_quality s
-    left join sbx_transcripts.allchats ac on ac.session_id = s.session_id
+    
+FROM dl_data_streetbees_api.submissions sub
+    left join sbx_transcripts.answer_quality s on CAST(sub.id AS varchar) = s.session_id
     left join wc on wc.sub_id = s.session_id
+    left join sbx_transcripts.allchats ac on ac.session_id = s.session_id
+
 
 where
     s.partition_0 in ('{{project name}}')
+group by 1,2,3,4,5
 order by 1;
